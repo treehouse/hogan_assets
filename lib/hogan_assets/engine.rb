7a6f076e6dbcb27e3a6f857c711c3a4966c99ea4
Andrew Olson <anolson@gmail.com>
2017-04-28 15:23:59 -0400

    Silence deprecations!

--- lib/hogan_assets/engine.rb
+++ lib/hogan_assets/engine.rb
@@ -4,7 +4,7 @@ module HoganAssets
       HoganAssets::Config.load_yml! if HoganAssets::Config.yml_exists?
       Rails.application.config.assets.configure do |env|
         HoganAssets::Config.template_extensions.each do |ext|
-          env.register_engine(".#{ext}", Tilt)
+          env.register_engine(".#{ext}", Tilt, silence_deprecation: true)
         end
       end
     end
