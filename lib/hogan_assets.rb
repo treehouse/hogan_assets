bb56f620292364297489a4472c0ba9376c33c2d3
Milo Winningham <milo@winningham.net>
2019-03-28 16:53:48 -0700

    Update for Sprockets 4 API

--- lib/hogan_assets.rb
+++ lib/hogan_assets.rb
@@ -13,7 +13,20 @@ module HoganAssets
     require 'sprockets'
     Config.load_yml! if Config.yml_exists?
     Config.template_extensions.each do |ext|
-      Sprockets.register_engine ".#{ext}", Tilt
+      register_extension(ext)
+    end
+  end
+
+  def self.register_extension(ext, env: Sprockets)
+    if env.respond_to?(:register_transformer)
+      env.register_mime_type "text/#{ext}", extensions: [".#{ext}"], charset: :css
+      env.register_preprocessor "text/#{ext}", Tilt
+    end
+
+    if env.respond_to?(:register_engine)
+      args = ["text/#{ext}", Tilt]
+      args << { mime_type: "text/#{ext}", silence_deprecation: true } if Sprockets::VERSION.start_with?("3")
+      env.register_engine(*args)
     end
   end
 end
--- lib/hogan_assets/engine.rb
+++ lib/hogan_assets/engine.rb
@@ -4,7 +4,7 @@ module HoganAssets
       HoganAssets::Config.load_yml! if HoganAssets::Config.yml_exists?
       Rails.application.config.assets.configure do |env|
         HoganAssets::Config.template_extensions.each do |ext|
-          env.register_engine(".#{ext}", Tilt, silence_deprecation: true)
+          HoganAssets.register_extension(ext, env: env)
         end
       end
     end
